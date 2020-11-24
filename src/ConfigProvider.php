859477e7f87c2a81b771bd5adec9f5e6aae30f6f
Xuan Yang <xuan.yang@goldenf.co>
2020-11-24 14:45:04 +0800

    fix autoload

---
+++ src/ConfigProvider.php
@@ -0,0 +1,36 @@
+<?php
+
+declare(strict_types=1);
+
+namespace GiocoPlus\EZAdmin;
+
+class ConfigProvider
+{
+    public function __invoke(): array
+    {
+        return [
+            'dependencies' => [
+                CacheInterface::class => Cache::class
+            ],
+            'commands' => [
+            ],
+            'listeners' => [
+
+            ],
+            // 合并到  config/autoload/annotations.php 文件
+            'annotations' => [
+                'scan' => [
+                    'paths' => [
+                        __DIR__,
+                    ],
+                ],
+            ],
+            'publish' => [
+                [
+                    'id' => 'ezadmin',
+                    'description' => 'ezadmin-tools'
+                ],
+            ],
+        ];
+    }
+}
