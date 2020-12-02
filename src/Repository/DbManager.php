fd78f985f16b63cd387f105422c4a0aadce3987f
Xuan Yang <xuan.yang@goldenf.co>
2020-12-02 12:01:17 +0800

    add op func connect postgresql

--- src/Repository/DbManager.php
+++ src/Repository/DbManager.php
@@ -30,13 +30,13 @@ class DbManager
     protected $mongodb;
 
     /**
-     * 選擇商戶資料庫
+     * 選擇商戶MongoDb資料庫
      * @param string $code
      * @param string|null $dbName
      * @param string $readPref
      * @return MongoDb
      */
-    public function setOperator(string $code, string $dbName = null, string $readPref = MongoDbConst::ReadPrefPrimary) {
+    public function opMongoDb(string $code, string $dbName = null, string $readPref = MongoDbConst::ReadPrefPrimary) {
         $dbName = $dbName ?? "{$code}_db";
         $op = $this->cache->operator($code);
         $dbConn = $op['db']->mongodb;
@@ -49,5 +49,28 @@ class DbManager
         $config = ApplicationContext::getContainer()->get(ConfigInterface::class);
         $config->set("mongodb.db_{$code}", $dbCfg);
         return $this->mongodb->setPool("db_{$code}");
+    }
+
+    /**
+     * 選擇商戶PostgreSql資料庫
+     * @param string $code
+     * @param string|null $dbName
+     * @return MongoDb
+     */
+    public function opPostgreDb(string $code, string $dbName = null) {
+        $op = $this->cache->operator($code);
+        $dbConn = $op['db']->postgres;
+        $host = $dbConn->host;
+        $port = $dbConn->port;
+        $dbName = $dbName ?? "{$code}_db";
+        $user = $dbConn->user;
+        $password = $dbConn->password;
+        $pg = new Swoole\Coroutine\PostgreSQL();
+        $conn = $pg->connect("host={$host} port={$port} dbname={$dbName} user={$user} password={$password}");
+        if (!$conn) {
+            var_dump($pg->error);
+            return;
+        }
+        return $pg;
     }
 }
