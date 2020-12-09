a342ebab9b10059c0463d87ca2589f85cead1e21
Xuan Yang <xuan.yang@goldenf.co>
2020-12-09 16:01:57 +0800

    add bypass roles

--- src/Middleware/BoIPCheckMiddleware.php
+++ src/Middleware/BoIPCheckMiddleware.php
@@ -59,7 +59,7 @@ class BoIPCheckMiddleware implements MiddlewareInterface
         if (stripos($request->getUri()->getPath(), '/api/v1/') !== false) {
             $comp = null;
             $userInfo = $this->jwt->getParserData();
-            if (trim(strtolower($userInfo['role'])) === 'supervisor') {
+            if (trim(strtolower($userInfo['role']), in_array(['supervisor', 'ops', 'dev']))) {
                 return $handler->handle($request);
             }
             $compCode =  $userInfo['company_code'] ?? "";
