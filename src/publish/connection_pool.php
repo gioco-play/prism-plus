ee6ef1b33029ef67a3c1fd6916fe383250802249
Xuan Yang <xuan.yang@goldenf.co>
2021-07-21 16:08:13 +0800

    add connection pool

--- src/publish/connection_pool.php
+++ src/publish/connection_pool.php
@@ -3,11 +3,11 @@
 return [
     'default' => [
         'pool' => [
-            'min_active'          => (int)env('CONNPOOL_MIN_ACTIVE', 20),
-            'max_active'          => (int)env('CONNPOOL_MAX_ACTIVE', 100),
-            'max_wait_time'       => (int)env('CONNPOOL_MAX_WAIT', 5),
-            'max_idle_time'       => (int)env('CONNPOOL_MAX_IDLE', 30),
-            'idle_check_interval' => (int)env('CONNPOOL_IDLE_CHECK_INTERVAL', 15),
+            'min_active'          => (int)env('CONN_POOL_MIN_ACTIVE', 20),
+            'max_active'          => (int)env('CONN_POOL_MAX_ACTIVE', 100),
+            'max_wait_time'       => (int)env('CONN_POOL_MAX_WAIT', 5),
+            'max_idle_time'       => (int)env('CONN_POOL_MAX_IDLE', 30),
+            'idle_check_interval' => (int)env('CONN_POOL_IDLE_CHECK_INTERVAL', 15),
         ],
     ],
 ];
