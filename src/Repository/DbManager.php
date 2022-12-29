3b5e234358e16a9dcba9383690f67662d4f0c964
q90016200 <q90016200@gmail.com>
2022-12-29 17:19:15 +0800

    update

--- src/Repository/DbManager.php
+++ src/Repository/DbManager.php
@@ -113,11 +113,16 @@ class DbManager
         $dbName = $dbName ?? strtolower("{$code}_db");
         //
         $pg = new \Swoole\Coroutine\PostgreSQL();
-        $conn = $pg->connect("host={$host} port={$port} dbname={$dbName} user={$user} password={$password}");
+        $pgConnect = "host={$host} port={$port} dbname={$dbName} user={$user} password={$password}";
+        $conn = $pg->connect($pgConnect);
         if (!$conn) {
             var_dump($pg->error);
-            throw new \Exception("[{$code}] Postgres 未連線成功");
-            return;
+
+            $conn = $pg->connect($pgConnect);
+            if (!$conn) {
+                throw new \Exception("[{$code}] Postgres 未連線成功");
+                return;
+            }
         }
         return $pg;
     }
